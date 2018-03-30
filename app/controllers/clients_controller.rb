b35692044784d72bdb28a207b90bba23212915ee
Jeff Ray <jeff.ray@firespring.com>
2018-03-30 11:04:37 -0500

    Add test video and client for iphone testing

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,9 +7,8 @@ class ClientsController < ApplicationController
   private
   
     def set_client
-      clients = ['steccap']
+      clients = ['steccap', 'test']
       @client = clients.detect { |c| c == params[:slug]}
-      logger.fatal @client.inspect
       raise ActionController::RoutingError.new('Not Found') unless @client.present?
     end
 end
