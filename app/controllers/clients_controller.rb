dfb9345c6ee1ea970f6de227eb789914ed30c6fd
Jeff Ray <jeff.ray@firespring.com>
2018-04-20 13:27:38 -0500

    Add la and test clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,7 +7,11 @@ class ClientsController < ApplicationController
   private
   
     def set_client
-      clients = [Client.new(name: 'STEC CAP', slug: 'steccap')]
+      clients = [
+        Client.new(name: 'STEC CAP', slug: 'steccap'), 
+        Client.new(name: 'Lincoln Arts Council', slug: 'la'),
+        Client.new(name: 'Test Client', slug: 'test')
+      ]
       @client = clients.detect { |c| c.slug == params[:slug]}
       raise ActionController::RoutingError.new('Not Found') unless @client.present?
     end
