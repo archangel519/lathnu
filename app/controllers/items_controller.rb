bf6cff059f84eab0ed186c5543758c21abb8a9d1
Jeff Ray <jeff.ray@firespring.com>
2020-01-28 13:58:46 -0600

    Add position and alternate item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,8 @@ class ItemsController < ApplicationController
   
     def set_item
       items = [
-        Item.new(name: 'Amulet', slug: 'amulet', gltf: 'scene.gltf', description: 'The Amulet of Vigor has mysterious powers.')
+        Item.new(name: 'Amulet', slug: 'amulet', gltf: 'scene.gltf', position: '0 0 0', scale: '.03 .03 .03', rotation: '0 0 230', description: 'The Amulet of Vigor has mysterious powers.'),
+        Item.new(name: 'Aether Sword', slug: 'aether-sword', gltf: 'scene.gltf', position: '.8 .2 -.2', scale: '.045 .045 .045', rotation: '0 130 0', description: 'A unique sword powered by aether.')
       ]
       @item = items.detect { |c| c.slug == params[:slug]}
       raise ActionController::RoutingError.new('Not Found') unless @item.present?
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item
   include ActiveModel::Model
   
-  attr_accessor :name, :slug, :gltf, :description
+  attr_accessor :name, :slug, :gltf, :scale, :position, :rotation, :description
 end
