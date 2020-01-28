515e6a9da6f8d5771423b34dd0c1e93ad89a9c37
Jeff Ray <jeff.ray@firespring.com>
2020-01-28 15:45:36 -0600

    Add viewer route to support all items without specific url

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,19 @@
-class ItemsController < ApplicationController
-  before_action :set_item, only: [:show]
-    
+class ItemsController < ApplicationController   
+  def viewer
+    @items = all_items
+  end
+  
   def show
+    @item = all_items.detect { |c| c.slug == params[:slug]}
+    raise ActionController::RoutingError.new('Not Found') unless @item.present?
   end
   
   private
   
-    def set_item
-      items = [
+    def all_items
+      [
         Item.new(name: 'Amulet', slug: 'amulet', gltf: 'scene.gltf', position: '0 0 0', scale: '.03 .03 .03', rotation: '0 0 230', description: 'The Amulet of Vigor has mysterious powers.'),
         Item.new(name: 'Aether Sword', slug: 'aether-sword', gltf: 'scene.gltf', position: '.8 .2 -.2', scale: '.045 .045 .045', rotation: '0 130 0', description: 'A unique sword powered by aether.')
       ]
-      @item = items.detect { |c| c.slug == params[:slug]}
-      raise ActionController::RoutingError.new('Not Found') unless @item.present?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'items#show'
   
+  get 'items/viewer', to: 'items#viewer'
   get 'items/:slug', to: 'items#show'
 end
