171b01f6de7d269d0a8206df16656194fa3ecdc4
Jeff Ray <jeff.ray@firespring.com>
2020-02-11 12:59:21 -0600

    Add Aetherhub Key

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,7 +13,8 @@ class ItemsController < ApplicationController
     def all_items
       [
         Item.new(name: 'Amulet', slug: 'amulet', gltf: 'scene.gltf', position: '0 0 0', scale: '.03 .03 .03', rotation: '0 0 230', description: 'The Amulet of Vigor has mysterious powers.'),
-        Item.new(name: 'Aether Sword', slug: 'aether-sword', gltf: 'scene.gltf', position: '.8 .2 -.2', scale: '.045 .045 .045', rotation: '0 130 0', description: 'A unique sword powered by aether.')
+        Item.new(name: 'Aether Sword', slug: 'aether-sword', gltf: 'scene.gltf', position: '.8 .2 -.2', scale: '.045 .045 .045', rotation: '0 130 0', description: 'A unique sword powered by aether.'),
+        Item.new(name: 'AetherHub Key', slug: 'aetherhub-key', gltf: 'scene.gltf', position: '0 0 0', scale: '.1 .1 .1', rotation: '0 0 0', description: 'A mysterious key.')
       ]
     end
 end
